474f1ebefb23e139d4489960759944b3264ab716
benben4567 <benben4567@gmail.com>
2022-02-14 22:16:05 +0700

    fix: ui

--- resources/views/pages/siswa/topik_tugas.blade.php
+++ resources/views/pages/siswa/topik_tugas.blade.php
@@ -16,7 +16,12 @@
                             <h5>Pertemuan {{ $meeting->meeting }}</h5>
                             @foreach ($meeting->assignments as $assignment )
                             <a role="button" target="_blank" href="{{ asset('storage/assignments/'.$assignment->file) }}" class="btn btn-danger btn-block mb-3"><i class="fas fa-file-pdf"></i> Download {{ $assignment->name }}</a>
-                            <button type="button" class="btn btn-outline-success">Upload Jawaban</button>
+                            <p id="assignment-answer">
+                                @if (!empty($assign))
+                                    <a href="{{ asset('storage/answers/'.$assign->file) }}" target="_blank" rel="noopener noreferrer">Hasil Tugas</a> - <small>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $assign->created_at)->format('d-m-Y H:i:s') }}</small>
+                                @endif
+                            </p>
+                            <button type="button" class="btn btn-outline-success btn-assignment" data-meeting="{{ $meeting->id }}">Upload Jawaban</button>
                             @endforeach
                         </div>
                     </div>
@@ -36,7 +41,12 @@
                             <h5>Pertemuan {{ $meeting->meeting }}</h5>
                             @foreach ($meeting->tasks as $task )
                             <a role="button" target="_blank" href="{{ asset('storage/tasks/'.$task->file) }}" class="btn btn-danger btn-block mb-3"><i class="fas fa-file-pdf"></i> Download {{ $task->name }}</a>
-                            <button type="button" class="btn btn-outline-success mb-4">Upload Jawaban</button>
+                            <p id="task-answer">
+                                @if (!empty($answer))
+                                    <a href="{{ asset('storage/answers/'.$answer->file) }}" target="_blank" rel="noopener noreferrer">Hasil Tugas</a> - <small>{{ \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $answer->created_at)->format('d-m-Y H:i:s') }}</small>
+                                @endif
+                            </p>
+                            <button type="button" class="btn btn-outline-success btn-task" data-meeting="{{ $meeting->id }}">Upload Jawaban</button>
                             @endforeach
                         </div>
                     </div>
@@ -58,34 +68,66 @@
         </div>
     </div>
 
-    <!-- Modal Tugas -->
-    <div class="modal fade" id="modalTugas" tabindex="-1" aria-labelledby="modalTugasLabel" aria-hidden="true">
+    <!-- Modal Assignment -->
+    <div class="modal fade" id="modalAssignment" tabindex="-1" aria-labelledby="modalAssignmentLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modalTugasLabel">Unggah Tugas</h5>
+                    <h5 class="modal-title" id="modalAssignmentLabel">Unggah Tugas Kelompok</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <div class="modal-body">
-                    <form action="" method="post">
+                <form action="" method="post" id="form-assignment">
+                    <div class="modal-body">
+                        <input type="text" class="d-none meeting-id"  name="id">
                         <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="customFile" accept=".pdf">
+                            <input type="file" class="custom-file-input" name="file" id="customFile" accept=".pdf">
                             <label class="custom-file-label" for="customFile" data-browse="Pilih">Pilih File</label>
                         </div>
-                    </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success">Unggah</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal Task -->
+    <div class="modal fade" id="modalTask" tabindex="-1" aria-labelledby="modalTaskLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTaskLabel">Unggah Tugas Individu</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-success">Unggah</button>
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                </div>
+                <form action="" method="post" id="form-task">
+                    <div class="modal-body">
+                        <input type="text" class="d-none meeting-id"  name="id">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" name="file" id="customFile" accept=".pdf">
+                            <label class="custom-file-label" for="customFile" data-browse="Pilih">Pilih File</label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success">Unggah</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 
 </div>
 @endsection
+
+@push('js-lib')
+<script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.js"></script>
+@endpush
 
 @push('js')
 <script src="{{asset('js/pages/siswa-topik-tugas.js')}}"></script>
