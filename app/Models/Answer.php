78cf40e12f6a85487f7d68fc4ada6d9c3894e935
Benben Bagus P <benben4567@gmail.com>
2022-05-11 14:52:08 +0700

    add: users relations

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -6,10 +6,15 @@ use Jenssegers\Mongodb\Eloquent\Model;
 
 class Answer extends Model
 {
-    protected $fillable = ['user_id', 'type', 'file'];
+    protected $fillable = ['user_id', 'group', 'individual'];
 
     public function meeting()
     {
         return $this->belongsTo(Meeting::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
